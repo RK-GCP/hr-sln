14dd35fa9ae56128acd0a9e7ba296e45d2013b42
Rashed Kamal <rashed.kamal@live.com>
2021-05-14 21:18:57 -0400

    plus-minus

--- plus-minus/main.go
+++ plus-minus/main.go
@@ -45,4 +45,31 @@ func main() {
 
 	fmt.Println(intArray)
 
+	plusminus(intArray)
+
 }
+
+func plusminus(nums []int32) bool {
+	result := false
+	n := len(nums)
+	positive := 0
+	negative := 0
+	zeroes := 0
+	for _, val := range nums {
+		if val < 0 {
+			negative++
+		} else if val > 0 {
+			positive++
+		} else {
+			zeroes++
+		}
+	}
+
+	fmt.Printf("%.6f\n", float64(positive)/float64(n))
+	fmt.Printf("%.6f\n", float64(negative)/float64(n))
+	fmt.Printf("%.6f\n", float64(zeroes)/float64(n))
+	result = true
+	return result
+}
+
+//https://www.hackerrank.com/challenges/plus-minus/problem
